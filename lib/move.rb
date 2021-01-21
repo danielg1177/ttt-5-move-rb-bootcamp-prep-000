3051fd0eae55bbee9108ff5cdb850e1aa6a5f60a
dauntless-strategy-1036 <danielgordon19972@gmail.com>
2021-01-21 14:12:03 +0000

    Automatically backed up by Learn

--- lib/move.rb
+++ lib/move.rb
@@ -6,8 +6,8 @@ def display_board(board)
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
 
-def input_to_index(move)
-  index = move.to_i - 1
+def input_to_index(input)
+  index = input.to_i - 1
 end
 
 def move (board, index, character = "X")
